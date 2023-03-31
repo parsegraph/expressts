0d44ef79e0fb60fbedf3ebcf41660d38d041fd2c
Aaron Faanes <dafrito@gmail.com>
2023-03-31 05:05:10 -0700

    server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,5 @@
 export const DIST_NAME = "expressts";
 import express from "express";
-import glob from "glob";
-
-async function getDemos(): Promise<string[]> {
-  return new Promise((respond, reject) => {
-    glob("../www/*.html", {}, function (err: any, files: string[]) {
-      if (err) {
-        reject(err);
-      }
-      // files is an array of filenames.
-      respond(
-        files.map((file: string) => {
-          const m = file.match(/www\/(\w+)\.html/);
-          [1];
-          return m ? m[1] : null;
-        })
-      );
-    });
-  });
-}
 
 export default (root: string) => {
   const app = express();
@@ -36,17 +17,9 @@ export default (root: string) => {
     write(`</head>`);
     write(`<body>`);
     write(
-      `<h1>${DIST_NAME} <a href='${root}/coverage/lcov-report/'>Coverage</a> <a href='${root}/docs'>Docs</a></h1>`
+      `<h1>${DIST_NAME} </h1>` +
+      `<a href='${root}/docs'>Typedoc</a>`
     );
-    write(
-      `<p>This library is available as a <a href="${root}/src/index.js">JavaScript UMD module</a></p>`
-    );
-    write(`<h2>Samples &amp; Demos</h2>`);
-    write(`<ul>`);
-    (await getDemos()).forEach((demo) => {
-      demo && write(`<li><a href='${root}/${demo}.html'>${demo}</li>`);
-    });
-    write(`</ul>`);
     write(`</body>`);
     write(`</html>`);
 
